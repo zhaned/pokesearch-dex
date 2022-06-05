aaa7716ce66b4703f45b36675f50e0a55a5cdee7
zhaned <zhanedmond@gmail.com>
2022-06-05 00:47:38 -0700

    created custom hook for conditional nav header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import Navigation from "./component/Navigation";
-import Home from './component/Homepage'
-import About from './component/About'
+import Home from "./component/Homepage";
+import About from "./component/About";
 import SearchPage from "./component/SearchPage";
+
+// updates nav existence upon entering a new route
+function useNav() {
+  let { pathname } = useLocation();
+  return pathname === "/" ? null : <Navigation />;
+}
 
 function App() {
   return (
     <div className="container">
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Search" element={<SearchPage />} />
-        </Routes>
-      </Router>
+      {useNav()}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Search" element={<SearchPage />} />
+      </Routes>
     </div>
   );
 }
--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -1,9 +1,11 @@
-import background from "../images/background.jpg"
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <div>
       <div>You are on the Home Page.</div>
+      <Link to='/Search'>Search</Link>
+      <Link to='/About'>About</Link>
     </div>
   );
 };
