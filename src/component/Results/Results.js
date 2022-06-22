ef63cad7863618251cca838022655481c1088e88
zhaned <zhanedmond@gmail.com>
2022-06-21 23:01:34 -0600

    fixed a styling error

--- src/component/Results/Results.js
+++ src/component/Results/Results.js
@@ -8,7 +8,12 @@ const Results = ({ pokemon }) => {
   const type2 = pokemon.types.length;
 
   return (
-    <div className="fade-in-above text-light">
+    <div
+      className="fade-in-above text-light"
+      style={{
+        textShadow: '2px 2px #851bed',
+      }}
+    >
       <div className="d-flex justify-content-center">
         <h1 className="display-3 text-center pt-1 pe-1">
           #{pokemon.id}{' '}
@@ -26,40 +31,47 @@ const Results = ({ pokemon }) => {
           opacity: '1',
         }}
       />
-      <div
-        className="d-flex justify-content-between"
-        style={{
+      <div>
+        <div className="d-flex justify-content-between"         style={{
           background:
             'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ), url(' +
             require(`../../images/types/${type}-bg.png`) +
             ') no-repeat ',
           backgroundPosition: 'center',
           backgroundSize: '100% 100%',
-          textShadow: '2px 2px #851bed',
-        }}
-      >
-        <Traits traits={pokemon}/>
-        <Stats stats={pokemon.stats}/>
-        <div
-          className="d-flex rounded-circle justify-content-center align-items-center"
-          style={{
-            objectFit: 'contain',
-            border: '1px solid #851bed',
-            background:
-              'linear-gradient(rgba(240, 240, 240, 0.35),rgba(15,15,15, 0.35) )',
-          }}
-        >
-          <img
-            src={pokemon.sprites.other['official-artwork'].front_default}
-            alt={pokemon.id}
-            className="img-fluid ms-1"
+        }}>
+          <Traits traits={pokemon} />
+          <Stats stats={pokemon.stats} />
+          <div
+            className="d-flex rounded-circle justify-content-center align-items-center"
             style={{
-              maxHeight: '84%',
+              objectFit: 'contain',
+              border: '1px solid #851bed',
+              background:
+                'linear-gradient(rgba(240, 240, 240, 0.35),rgba(15,15,15, 0.35) )',
             }}
-          />
+          >
+            <img
+              src={pokemon.sprites.other['official-artwork'].front_default}
+              alt={pokemon.id}
+              className="img-fluid ms-1"
+              style={{
+                maxHeight: '84%',
+              }}
+            />
+          </div>
+        </div>
+        <hr
+          style={{
+            border: '1px solid #f0f0f0',
+            borderRadius: '2px',
+            opacity: '1',
+          }}
+        />
+        <div>
+          <Moveset moves={pokemon.moves} />
         </div>
       </div>
-        <Moveset moves={pokemon.moves} />
     </div>
   );
 };
